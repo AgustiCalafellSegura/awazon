e438b73349f503209cab03245b4172a4c89dbb77
Agusti Calafell <agusticalafellsegura@gmail.com>
2018-02-25 18:06:03 +0100

    Repository

---
+++ src/Repository/OrderRepository.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: agusti
+ * Date: 20/02/18
+ * Time: 16:35
+ */
+
+namespace App\Repository;
+
+
+use Doctrine\ORM\EntityRepository;
+
+class OrderRepository extends EntityRepository
+{
+    public function findAllSortedByNameQB()
+    {
+        return $this->createQueryBuilder('o')
+            ->orderBy('o.date', 'ASC')
+            ;
+    }
+    public function findAllSortedByNameQ()
+    {
+        return $this->findAllSortedByNameQB()->getQuery();
+    }
+    public function findAllSortedByName()
+    {
+        return $this->findAllSortedByNameQ()->getResult();
+    }
+}
---
+++ src/Repository/ProductRepository.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: agusti
+ * Date: 20/02/18
+ * Time: 16:35
+ */
+
+namespace App\Repository;
+
+
+use Doctrine\ORM\EntityRepository;
+
+class ProductRepository extends EntityRepository
+{
+    public function findAllSortedByNameQB()
+    {
+        return $this->createQueryBuilder('p')
+            ->orderBy('p.name', 'ASC')
+            ;
+    }
+    public function findAllSortedByNameQ()
+    {
+        return $this->findAllSortedByNameQB()->getQuery();
+    }
+    public function findAllSortedByName()
+    {
+        return $this->findAllSortedByNameQ()->getResult();
+    }
+}
