f2bd36b79727013322f665b37a1e551b4601d15a
Agusti Calafell <agusticalafellsegura@gmail.com>
2018-03-06 17:55:08 +0100

    Improve CustomerRepository

--- src/Repository/CustomerRepository.php
+++ src/Repository/CustomerRepository.php
@@ -13,18 +13,62 @@ use Doctrine\ORM\EntityRepository;
 
 class CustomerRepository extends EntityRepository
 {
+    /**
+     * @return \Doctrine\ORM\QueryBuilder
+     */
     public function findAllSortedByNameQB()
     {
         return $this->createQueryBuilder('p')
             ->orderBy('p.name', 'ASC')
             ;
     }
+
+    /**
+     * @return \Doctrine\ORM\Query
+     */
     public function findAllSortedByNameQ()
     {
         return $this->findAllSortedByNameQB()->getQuery();
     }
+
+    /**
+     * @return array
+     */
     public function findAllSortedByName()
     {
         return $this->findAllSortedByNameQ()->getResult();
+    }
+
+    /**
+     * @return \Doctrine\ORM\QueryBuilder
+     */
+    public function findBestCustomersQB()
+    {
+        $qb = $this->createQueryBuilder('c')
+            ->select('COUNT(c.id) as amount')
+            ->join('c.orders', 'o')
+            ->groupBy('o.customer')
+            ->setMaxResults(10)
+            ->orderBy('c.name', 'ASC')
+            ->addOrderBy('c.createdAt', 'DESC')
+        ;
+
+        return $qb;
+    }
+
+    /**
+     * @return \Doctrine\ORM\Query
+     */
+    public function findBestsCustomersQ()
+    {
+        return $this->findBestCustomersQB()->getQuery();
+    }
+
+    /**
+     * @return array
+     */
+    public function findBestCustomers()
+    {
+        return $this->findBestsCustomersQ()->getResult();
     }
 }
