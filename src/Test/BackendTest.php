77a6ce965c245b0b538edfff06a74b176e716b2a
Agusti Calafell <agusticalafellsegura@gmail.com>
2018-03-27 16:54:58 +0200

    Add FrontendTest

---
+++ src/Test/BackendTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: agusti
+ * Date: 27/03/18
+ * Time: 16:50
+ */
+
+namespace App\Test;
+
+use Liip\FunctionalTestBundle\Test\WebTestCase;
+
+class BackendTest extends WebTestCase
+{
+
+}
