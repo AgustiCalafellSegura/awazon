efda46f7528e64df9322a6a69726c94094385d9e
Ruben Hierro <rubenhierro@gmail.com>
2018-04-24 11:44:57 +0200

    new migration

---
+++ src/Migrations/Version20180424091902.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180424091902 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE Customers ADD slug VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE Providers ADD slug VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE Customers DROP slug');
+        $this->addSql('ALTER TABLE Providers DROP slug');
+    }
+}
