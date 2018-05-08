4ccd15df8d9e1ff8250d1b7d17f9b3e9e1071c84
Agusti Calafell <agusticalafellsegura@gmail.com>
2018-05-08 16:19:12 +0200

    Improve run-test again

--- scripts/developer/run-tests.sh
+++ scripts/developer/run-tests.sh
@@ -6,6 +6,7 @@ echo "Started at `date +"%T %d/%m/%Y"`"
 ./bin/console ca:cl --env=test
 ./bin/console doctrine:database:drop --force --env=test
 ./bin/console doctrine:database:create --env=test
+./bin/console doctrine:schema:update --force --env=test
 ./bin/console hautelook:fixtures:load --no-interaction --env=test
 ./bin/phpunit
 
