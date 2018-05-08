fe8478df30fbc8035c19a9e34a3107ed22568cfa
Agusti Calafell <agusticalafellsegura@gmail.com>
2018-05-08 16:14:48 +0200

    Improve run-test

--- scripts/developer/run-tests.sh
+++ scripts/developer/run-tests.sh
@@ -3,6 +3,10 @@
 echo "Excuting test suite."
 echo "Started at `date +"%T %d/%m/%Y"`"
 
+./bin/console ca:cl --env=test
+./bin/console doctrine:database:drop --force --env=test
+./bin/console doctrine:database:create --env=test
+./bin/console hautelook:fixtures:load --no-interaction --env=test
 ./bin/phpunit
 
 echo "Finished at `date +"%T %d/%m/%Y"`"
