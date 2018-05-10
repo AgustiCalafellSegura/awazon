dab9d61822f5f3e64fb2247b08e195023d1e5e6e
Ruben Hierro <rubenhierro@gmail.com>
2018-05-10 18:33:53 +0200

    Improve Product controller

--- src/Controller/Frontend/ProductController.php
+++ src/Controller/Frontend/ProductController.php
@@ -3,7 +3,6 @@
 namespace App\Controller\Frontend;
 
 use App\Entity\Product;
-use App\Entity\Rating;
 use App\Form\ProductFormType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -17,6 +16,10 @@ class ProductController extends Controller
 {
     /**
      * @Route("/product/{slug}", name="app_frontend_product_detail")
+     *
+     * @param string $slug
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function productDetail($slug)
     {
@@ -25,39 +28,12 @@ class ProductController extends Controller
         if (is_null($product)) {
             throw new NotFoundHttpException();
         }
-        $counter1 = 0;
-        $counter2 = 0;
-        $counter3 = 0;
-        $counter4 = 0;
-        $counter5 = 0;
-        $totalRatings = count($product->getRatings());
 
-        /** @var Rating $rate */
-        foreach ($product->getRatings() as $rate) {
-            if (1 == $rate->getRate()) {
-                ++$counter1;
-            } elseif (2 == $rate->getRate()) {
-                ++$counter2;
-            } elseif (3 == $rate->getRate()) {
-                ++$counter3;
-            } elseif (4 == $rate->getRate()) {
-                ++$counter4;
-            } elseif (5 == $rate->getRate()) {
-                ++$counter5;
-            }
-        }
-
-        if ($totalRatings > 0) {
-            $counter1 = (($counter1 * 100) / $totalRatings);
-            $counter2 = (($counter2 * 100) / $totalRatings);
-            $counter3 = (($counter3 * 100) / $totalRatings);
-            $counter4 = (($counter4 * 100) / $totalRatings);
-            $counter5 = (($counter5 * 100) / $totalRatings);
-        }
+        $counter = $this->get('app.frequency_counter.manager')->getStarsFrequencyPercentile($product->getRatings());
 
         return $this->render('frontend/product/detail.html.twig', array(
             'product' => $product,
-            'ratecounter' => array($counter1, $counter2, $counter3, $counter4, $counter5),
+            'ratecounter' => $counter,
         ));
     }
 
