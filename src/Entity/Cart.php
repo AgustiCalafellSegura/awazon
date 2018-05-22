7f1f1b03854e2d35c87d174e0f1b9b7c21a92b0d
Ruben Hierro <rubenhierro@gmail.com>
2018-05-22 17:30:45 +0200

    Create Cart and CartItem entities

---
+++ src/Entity/Cart.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Class Cart.
+ */
+class Cart extends AbstractBase
+{
+    /**
+     * @var CartItem[]
+     */
+    private $items;
+
+    /**
+     * Cart constructor.
+     */
+    public function __construct()
+    {
+        $this->items = new ArrayCollection();
+    }
+
+    /**
+     * @return CartItem[]
+     */
+    public function getItems()
+    {
+        return $this->items;
+    }
+
+    /**
+     * @param CartItem[] $items
+     *
+     * @return $this
+     */
+    public function setItems($items)
+    {
+        $this->items = $items;
+
+        return $this;
+    }
+
+    /**
+     * @param CartItem $item
+     *
+     * @return $this
+     */
+    public function addItem(CartItem $item)
+    {
+        if (!$this->items->contains($item)) {
+            $this->items->add($item);
+            $item->setCart($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param CartItem $item
+     *
+     * @return $this
+     */
+    public function removeItem(CartItem $item)
+    {
+        if ($this->items->contains($item)) {
+            $this->items->remove($item);
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/CartItem.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Entity;
+
+/**
+ * Class CartItem.
+ */
+class CartItem extends AbstractBase
+{
+    /**
+     * @var Product
+     */
+    private $product;
+
+    /**
+     * @var int
+     */
+    private $units;
+
+    /**
+     * @var Cart
+     */
+    private $cart;
+
+    /**
+     * @return Product
+     */
+    public function getProduct()
+    {
+        return $this->product;
+    }
+
+    /**
+     * @param Product $product
+     *
+     * @return $this
+     */
+    public function setProduct($product)
+    {
+        $this->product = $product;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getUnits()
+    {
+        return $this->units;
+    }
+
+    /**
+     * @param int $units
+     *
+     * @return $this
+     */
+    public function setUnits($units)
+    {
+        $this->units = $units;
+
+        return $this;
+    }
+
+    /**
+     * @return Cart
+     */
+    public function getCart()
+    {
+        return $this->cart;
+    }
+
+    /**
+     * @param Cart $cart
+     *
+     * @return $this
+     */
+    public function setCart($cart)
+    {
+        $this->cart = $cart;
+
+        return $this;
+    }
+}
